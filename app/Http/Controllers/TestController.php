5b9b6b2210367b9b99b3d6a18b34834eb7da1d26
LucasLaurens <lucaslaurens.74@gmail.com>
2021-08-22 13:17:41 +0200

    add a gate to access any page

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class TestController extends Controller
+{
+    public function foo() {
+        // Define in the AuthServiceProvider file
+        if (! Gate::allows('access-admin')) {
+            abort(403);
+        }
+
+        return view('test.foo');
+    }
+
+    public function bar() {
+        return view('test.bar');
+    }
+}
---
+++ resources/views/test/bar.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Bar')
+
+@section('content')
+    @parent
+    
+    <div>
+        <h3>Bar</h3>
+    </div>
+@endsection
+
+
