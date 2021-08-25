98fad5963067ea80043716e2930962847fe4a0b9
LucasLaurens <lucaslaurens.74@gmail.com>
2021-08-25 12:07:51 +0200

    add style and format to the blog

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @yield('content')
--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -8,13 +8,24 @@
     <div>
         <h3>Home</h3>
 
-        <div>
+        @foreach ($categories as $category)
+            <div class="card-category">
+                <div class="card-category-title"><strong>{{ $category->title }}</strong></div>
+            </div>
+        @endforeach
+        
+        <div class="cards">
             @foreach ($categories as $category)
-                <div><strong>{{ $category->title }}</strong></div>
                 @foreach ($category->posts as $post)
-                    <div>{{ $post->title }}</div>
-                    <div>{{ $post->description }}</div>
-                    <div>{{ $post->created_at }}</div>
+                    <div class="card-post">
+                        <div class="card-post-title"><strong>{{ $post->title }}</strong></div>
+                        <div class="card-post-description">
+                            {{ Str::limit($post->description, 100, ' ...') }}
+                        </div>
+                        <div class="card-post-time">
+                            <time>The post was created on {{ \Carbon\Carbon::parse($post->created_at)->format('Y-m-d') }}</time>
+                        </div>
+                    </div>
                 @endforeach
             @endforeach
         </div>
