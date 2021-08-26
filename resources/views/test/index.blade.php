fbf5bfe91d455c3baf9d776c5b953e27a6585a6f
LucasLaurens <lucaslaurens.74@gmail.com>
2021-08-26 09:32:44 +0200

    add soft deletes

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -17,14 +17,25 @@
         <div class="cards">
             @foreach ($categories as $category)
                 @foreach ($category->posts as $post)
-                    <div class="card-post">
+                <div class="card-post">
                         <div class="card-post-title"><strong>{{ $post->title }}</strong></div>
+                        @if(isset($post->deleted_at))
+                            <div><strong style="color: #FF5555;">Has been deleted</strong></div>
+                        @endif
                         <div class="card-post-description">
                             {{ Str::limit($post->description, 100, ' ...') }}
                         </div>
                         <div class="card-post-time">
                             <time>The post was created on {{ \Carbon\Carbon::parse($post->created_at)->format('Y-m-d') }}</time>
                         </div>
+                        <form method="POST" action="{{ route('post.delete', ['id' => $post->id]) }}">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                    
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-danger delete-post" value="Delete post">
+                            </div>
+                        </form>
                     </div>
                 @endforeach
             @endforeach
