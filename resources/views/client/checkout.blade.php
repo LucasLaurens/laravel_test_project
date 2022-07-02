580fb6de49d2699f91c39e8edf85862c98982471
LucasLaurens <lucaslaurens.74@gmail.com>
2022-07-02 15:15:42 +0200

    ensure the customer can pay and get his invoice associated

--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -5,58 +5,249 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Checkout</title>
+
+    <style>
+        /* Variables */
+        * {
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+            font-size: 16px;
+            -webkit-font-smoothing: antialiased;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+            height: 100vh;
+            width: 100vw;
+        }
+
+        form {
+            width: 30vw;
+            min-width: 500px;
+            align-self: center;
+            box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
+                0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+            border-radius: 7px;
+            padding: 40px;
+        }
+
+        .hidden {
+            display: none;
+        }
+
+        #payment-message {
+            color: rgb(105, 115, 134);
+            font-size: 16px;
+            line-height: 20px;
+            padding-top: 12px;
+            text-align: center;
+        }
+
+        #payment-element {
+            margin-bottom: 24px;
+        }
+
+        /* Buttons and links */
+        button {
+            background: #5469d4;
+            font-family: Arial, sans-serif;
+            color: #ffffff;
+            border-radius: 4px;
+            border: 0;
+            padding: 12px 16px;
+            font-size: 16px;
+            font-weight: 600;
+            cursor: pointer;
+            display: block;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+            width: 100%;
+        }
+        button:hover {
+            filter: contrast(115%);
+        }
+        button:disabled {
+            opacity: 0.5;
+            cursor: default;
+        }
+
+        /* spinner/processing state, errors */
+        .spinner,
+        .spinner:before,
+        .spinner:after {
+            border-radius: 50%;
+        }
+        .spinner {
+            color: #ffffff;
+            font-size: 22px;
+            text-indent: -99999px;
+            margin: 0px auto;
+            position: relative;
+            width: 20px;
+            height: 20px;
+            box-shadow: inset 0 0 0 2px;
+            -webkit-transform: translateZ(0);
+            -ms-transform: translateZ(0);
+            transform: translateZ(0);
+        }
+        .spinner:before,
+        .spinner:after {
+            position: absolute;
+            content: "";
+        }
+        .spinner:before {
+            width: 10.4px;
+            height: 20.4px;
+            background: #5469d4;
+            border-radius: 20.4px 0 0 20.4px;
+            top: -0.2px;
+            left: -0.2px;
+            -webkit-transform-origin: 10.4px 10.2px;
+            transform-origin: 10.4px 10.2px;
+            -webkit-animation: loading 2s infinite ease 1.5s;
+            animation: loading 2s infinite ease 1.5s;
+        }
+        .spinner:after {
+            width: 10.4px;
+            height: 10.2px;
+            background: #5469d4;
+            border-radius: 0 10.2px 10.2px 0;
+            top: -0.1px;
+            left: 10.2px;
+            -webkit-transform-origin: 0px 10.2px;
+            transform-origin: 0px 10.2px;
+            -webkit-animation: loading 2s infinite ease;
+            animation: loading 2s infinite ease;
+        }
+
+        .mt-10 {
+            margin-top: 10rem;
+        }
+
+        @-webkit-keyframes loading {
+            0% {
+                -webkit-transform: rotate(0deg);
+                transform: rotate(0deg);
+            }
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+            }
+        }
+        @keyframes loading {
+            0% {
+                -webkit-transform: rotate(0deg);
+                transform: rotate(0deg);
+            }
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+            }
+        }
+
+        @media only screen and (max-width: 600px) {
+            form {
+                width: 80vw;
+                min-width: initial;
+            }
+        }
+    </style>
 </head>
 <body>
-    <form action="{{ route('payment') }}">
-        @csrf
-        <input id="payment_method" name="payment_method" type="hidden">
-        <input id="card-holder-name" type="text">
+    <div class="mt-10 grid grid-column">
+        <form action="{{ route('payment') }}" method="post" id="payment-form">
+            @csrf
+            <input id="payment_method" name="payment_method" type="hidden">
+            <input id="card-holder-name" type="hidden" value="{{ auth()->user() }}">
 
-        <!-- Stripe Elements Placeholder -->
-        <div id="card-element"></div>
+            <!-- Stripe Elements Placeholder -->
+            <div id="payment-element"></div>
 
-        <button id="card-button">
-            Process Payment
-        </button>
-    </form>
+            <button id="paymentSubmit" type="submit">
+                <div class="spinner hidden" id="spinner"></div>
+                <span id="button-text">Pay now</span>
+            </button>
 
+            <div id="payment-message" class="hidden"></div>
+        </form>
+
+        <form class="mt-10" action="{{ route('last.invoice') }}" method="get">
+            <button>Download the last invoice</button>
+        </form>
+    </div>
 
     <script src="https://js.stripe.com/v3/"></script>
 
     <script>
-        const stripe = Stripe(' {{ env('STRIPE_KEY') }} ');
+
+        const clientSecret = '{{ env('STRIPE_KEY') }}';
+        const stripe = Stripe(clientSecret);
 
         const elements = stripe.elements();
-        const cardElement = elements.create('card', {
-            classes: {
-                base: 'StripeElement bg-white w-1/2 p-2 my-2 rounded-lg'
-            }
+        const paymentElement = elements.create('card', {
+            hidePostalCode: true,
         });
 
-        cardElement.mount('#card-element');
+        paymentElement.mount('#payment-element');
 
         const cardHolderName = document.getElementById('card-holder-name');
-        const cardButton = document.getElementById('card-button');
+        const form = document.getElementById('payment-form');
+        const paymentSubmit = document.getElementById('paymentSubmit');
 
-        cardButton.addEventListener('click', async (e) => {
+        paymentSubmit.addEventListener('click', async (e) => {
             e.preventDefault();
+            setLoading(true);
 
             const { paymentMethod, error } = await stripe.createPaymentMethod(
-                'card', cardElement, {
+                'card', paymentElement, {
                     billing_details: { name: cardHolderName.value }
                 }
             );
 
+            setLoading(false);
+
             if (error) {
                 // Display "error.message" to the user...
-                alert(error);
+                showMessage(error.message);
             } else {
                 // The card has been verified successfully...
-                document.getElementById('payment_method').value = paymentMethod.id;
+                try {
+                    document.getElementById('payment_method').value = paymentMethod.id;
+                    form.submit();
+                } catch (err) {
+                    console.log(err)
+                }
             }
 
-            document.querySelector('form').submit();
         });
+
+        function showMessage(messageText) {
+            const messageContainer = document.querySelector("#payment-message");
+
+            messageContainer.classList.remove("hidden");
+            messageContainer.textContent = messageText;
+
+            setTimeout(function () {
+                messageContainer.classList.add("hidden");
+                messageText.textContent = "";
+            }, 4000);
+        }
+
+        // Show a spinner on payment submission
+        function setLoading(isLoading) {
+            if (isLoading) {
+                // Disable the button and show a spinner
+                document.querySelector("#paymentSubmit").disabled = true;
+                document.querySelector("#spinner").classList.remove("hidden");
+                document.querySelector("#button-text").classList.add("hidden");
+            } else {
+                document.querySelector("#paymentSubmit").disabled = false;
+                document.querySelector("#spinner").classList.add("hidden");
+                document.querySelector("#button-text").classList.remove("hidden");
+            }
+        }
     </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 // use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Cashier\Cashier;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,14 +29,35 @@ Route::middleware(['auth'])->group(function() {
         return view('client.checkout');
     })->name('checkout');
 
-    Route::get('/payment', function (Request $request) {
+    Route::post('/payment', function (Request $request) {
         $user = auth()->user();
-        $stripeCharge = $user->charge(
-            100, $request->payment_method
+        $user->createOrGetStripeCustomer();
+
+        $paymentMethod = $request->payment_method;
+        $user->updateDefaultPaymentMethod($request->payment_method);
+        $amount = 2000;
+        $productName = "test";
+
+        $user->charge(
+            $amount, $paymentMethod
         );
 
-        dd($stripeCharge);
+        $user->invoiceFor(
+            $productName, $amount
+        );
+
+        return redirect()->route('checkout');
     })->name('payment');
+
+    Route::get('/last/invoice', function () {
+        $user = auth()->user();
+        $user->createOrGetStripeCustomer();
+
+        return $user->downloadInvoice($user->invoices()->last()->id, [
+            'vendor' => 'Your Company',
+            'product' => 'Your Product',
+        ]);
+    })->name('last.invoice');
 });
 
 require __DIR__.'/auth.php';
